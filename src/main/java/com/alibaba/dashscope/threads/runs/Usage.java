1dd06c3f4b1dedaef0713ea0dfd11e5ea1e78621
Kevin Lin <gucheng02061152@163.com>
2025-07-20 17:40:41 +0800

    feat(assistant_api): support more usage info

--- src/main/java/com/alibaba/dashscope/threads/runs/Usage.java
+++ src/main/java/com/alibaba/dashscope/threads/runs/Usage.java
@@ -14,6 +14,12 @@ public final class Usage {
   @SerializedName("completion_tokens")
   private Integer completionTokens;
 
+  @SerializedName("input_tokens")
+  private Integer inputTokens;
+
+  @SerializedName("output_tokens")
+  private Integer outputTokens;
+
   @SerializedName("total_tokens")
   private Integer totalTokens;
 }
